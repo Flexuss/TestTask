8d55ed15220ac45b5bf94b4d6642d80052a605c1
Flexuss <flexusssss@gmail.com>
2017-09-21 11:08:53 +0300

    bug fix

--- src/main/java/com/ivanovdmitry/RestApiController.java
+++ src/main/java/com/ivanovdmitry/RestApiController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Dmitry on 20.09.2017.
  */
@@ -21,6 +24,9 @@ public class RestApiController {
     @RequestMapping(value = "/authorization", method = RequestMethod.POST)
     public Response authorization(@RequestParam(value = "username") String username,
                                   @RequestParam(value = "password") String password){
+        if(!checkParam(username)){
+            return new Response("Username is empty");
+        }
         User user=userService.findByUsername(username);
         if(user==null){
             return new Response("User not found");
@@ -34,10 +40,22 @@ public class RestApiController {
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public Response registration(@RequestParam(value = "username") String username,
                                  @RequestParam(value = "password") String password){
+        if(!checkParam(username)){
+            return new Response("Username is empty");
+        }
         User user=userService.findByUsername(username);
         if(user==null){
+            if(!checkParam(password)){
+                return new Response("Password is empty");
+            }
             userService.save(new User(username, password));
             return new Response("Success");
         }else return new Response("User already exist");
+    }
+
+    public static boolean checkParam(String param){
+        Pattern p = Pattern.compile("^[a-zA-Z0-9]+$");
+        Matcher m = p.matcher(param);
+        return m.matches();
     }
 }
